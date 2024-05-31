be565009bbbd599db98db4bcdab8067ac9b3ae3c
Kayla C <itskaychay@gmail.com>
2024-05-31 18:12:32 -0400

    Updated users db

--- mongo-db/users-db.js
+++ mongo-db/users-db.js
@@ -6,11 +6,23 @@ var user1 = {
   Password: "12345",
   Email: "user1@email.com",
   Birthday: "0000-00-0",
-  FavoriteMovies: ["Iron Man"],
+  FavoriteMovies: [ObjectId("6656572676c29aee626387b8")],
 };
 
 db.users.insertOne(user1);
 ObjectId("66577ce3d42075717c3404db");
+
+db.users.updateOne(
+  { _id: ObjectId("66577ce3d42075717c3404db") },
+  { $set: { Birthday: "Jan 1, 2001" } }
+);
+
+db.users.updateOne(
+  { _id: ObjectId("66577ce3d42075717c3404db") },
+  { $set: { FavoriteMovies: ObjectId("6656572676c29aee626387b8") } }
+);
+
+db.users.findOne(ObjectId("66577ce3d42075717c3404db"));
 
 // user2
 var user2 = {
@@ -18,11 +30,21 @@ var user2 = {
   Password: "12345",
   Email: "user2@email.com",
   Birthday: "0000-00-0",
-  FavoriteMovies: ["Iron Man 2"],
+  FavoriteMovies: [ObjectId("6656577376c29aee626387ba")],
 };
 
 db.users.insertOne(user2);
 ObjectId("66577ceed42075717c3404dc");
+
+db.users.updateOne(
+  { _id: ObjectId("66577ceed42075717c3404dc") },
+  { $set: { Birthday: "Jan 1, 2001" } }
+);
+
+db.users.updateOne(
+  { _id: ObjectId("66577ceed42075717c3404dc") },
+  { $set: { FavoriteMovies: ObjectId("6656577376c29aee626387ba") } }
+);
 
 // user3
 var user3 = {
@@ -30,8 +52,20 @@ var user3 = {
   Password: "12345",
   Email: "user3@email.com",
   Birthday: "0000-00-0",
-  FavoriteMovies: ["Iron Man 3"],
+  FavoriteMovies: [ObjectId("66566168f065c4f12c325430")],
 };
 
 db.users.insertOne(user3);
 ObjectId("66577cf7d42075717c3404dd");
+
+db.users.updateOne(
+  { _id: ObjectId("66577cf7d42075717c3404dd") },
+  { $set: { Birthday: "Jan 1, 2001" } }
+);
+
+db.users.updateOne(
+  { _id: ObjectId("66577cf7d42075717c3404dd") },
+  { $set: { FavoriteMovies: ObjectId("66566168f065c4f12c325430") } }
+);
+
+db.users.findOne(ObjectId("66577cf7d42075717c3404dd"));
