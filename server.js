8921f3b4fab423375df229860a7240c7d2b2db79
Kayla C <itskaychay@gmail.com>
2024-06-07 16:13:44 -0400

    updated uri

--- server.js
+++ server.js
@@ -18,20 +18,17 @@ const Genres = Models.Genre; // Genre model
 // server-side validation
 const { check, validationResult } = require("express-validator"); // importing express-validator package
 
-// connecting to mongodb
-// mongoose.connect(
-//   "mongodb+srv://kayashchan:qxJarHH5h0KcGazJ@mcu-marvel-movie-db.k06zdqt.mongodb.net/?retryWrites=true&w=majority",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-
-// Mongoose connects to db, "MCUmarvel-movie-api-db"
-mongoose.connect("mongodb://127.0.0.1:27017/MCUmarvel-movie-api-db", {
+// connecting to online db, Mongodb Atlas
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Mongoose connects to local db, "MCUmarvel-movie-api-db"
+// mongoose.connect("mongodb://127.0.0.1:27017/MCUmarvel-movie-api-db", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 // app = express functionality
 const app = express(); // to config to web server
